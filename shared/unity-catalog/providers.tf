0e0a47631a48a34d26a4b0133a5ad35d7ac72090
myticBliss <saqib.mj@gmail.com>
2024-09-17 16:55:20 +0400

    optimized codebase

--- shared/unity-catalog/providers.tf
+++ shared/unity-catalog/providers.tf
@@ -2,10 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = "4.2.0"
     }
     databricks = {
       source = "databricks/databricks"
+      version = "1.51.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "CP-CLOUD-SUPPORT"
+    storage_account_name = "cpcloudsupport2tfstates"
+    container_name       = "databricks-multienv-poc"
+    key                  = "terraform.tfstate"
   }
 }
 
