2b111a8505626f48c1c5dc7cabb796148f8fcb51
myticBliss <saqib.mj@gmail.com>
2024-09-17 09:10:02 +0400

    remove multiple authentication

--- shared/unity-catalog/providers.tf
+++ shared/unity-catalog/providers.tf
@@ -29,8 +29,6 @@ provider "databricks" {
   alias         = "account"
   host          = "https://accounts.azuredatabricks.net"
   account_id    = var.databricks_account_id
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
 
 
 }
